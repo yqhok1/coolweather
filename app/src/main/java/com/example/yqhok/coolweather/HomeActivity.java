1512cce52605b4be5b60154e2700399c4bbbf8d6
York <yqhok1@outlook.com>
2017-05-24 10:48:21 +0800

    Add new acitivities and layouts

--- app/src/main/java/com/example/yqhok/coolweather/HomeActivity.java
+++ app/src/main/java/com/example/yqhok/coolweather/HomeActivity.java
@@ -1,13 +1,27 @@
 package com.example.yqhok.coolweather;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 
-public class HomeActivity extends AppCompatActivity {
+import com.example.yqhok.coolweather.base.BaseActivity;
+import com.example.yqhok.coolweather.databinding.ActivityBaseBinding;
+
+public class HomeActivity extends BaseActivity<ActivityBaseBinding> {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setTitle("Demo");
+        showContentView();
+        initView();
     }
+
+    private void initView() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(false);
+        }
+    }
+
 }
