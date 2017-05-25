0e1f3838fe2b0245bc9d748a4a330ef83bfce49e
York <yqhok1@outlook.com>
2017-05-25 22:32:12 +0800

    Bug fixes

--- app/src/main/java/com/example/yqhok/coolweather/SettingsActivity.java
+++ app/src/main/java/com/example/yqhok/coolweather/SettingsActivity.java
@@ -9,17 +9,24 @@ import com.example.yqhok.coolweather.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends BaseActivity<ActivitySettingsBinding> {
 
+    private SettingsFragment settingsFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
         setTitle("设置");
+        initView();
         showContentView();
     }
 
     public static void start(Context context) {
         Intent intent = new Intent(context, SettingsActivity.class);
         context.startActivity(intent);
+    }
+
+    private void initView() {
+        //getFragmentManager().beginTransaction().replace(R.id.settings_frame_layout, settingsFragment).commit();
     }
 
 }
