b3ed3bf9b506336ffe2e495cf6302863e4115d62
York <yqhok1@outlook.com>
2017-05-26 23:31:56 +0800

    Add some new features.

---
+++ app/src/main/java/com/example/yqhok/coolweather/MyApplication.java
@@ -0,0 +1,25 @@
+package com.example.yqhok.coolweather;
+
+import android.content.Context;
+
+import org.litepal.LitePalApplication;
+
+/**
+ * Created by yqhok on 2017/5/26.
+ */
+
+public class MyApplication extends LitePalApplication {
+
+    private static Context context;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        context = getApplicationContext();
+        LitePalApplication.initialize(context);
+    }
+
+    public static Context getContext() {
+        return context;
+    }
+}
--- app/src/main/java/com/example/yqhok/coolweather/SettingsActivity.java
+++ app/src/main/java/com/example/yqhok/coolweather/SettingsActivity.java
@@ -1,20 +1,27 @@
 package com.example.yqhok.coolweather;
 
+import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.support.annotation.Nullable;
+import android.widget.TimePicker;
 
 import com.example.yqhok.coolweather.base.BaseActivity;
 import com.example.yqhok.coolweather.databinding.ActivitySettingsBinding;
 
 public class SettingsActivity extends BaseActivity<ActivitySettingsBinding> {
 
-    private SettingsFragment settingsFragment;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+        final SettingsFragment settingsFragment = new SettingsFragment();
+        getFragmentManager().beginTransaction().replace(R.id.settings_frame_layout, settingsFragment).commit();
         setTitle("设置");
         initView();
         showContentView();
@@ -26,7 +33,104 @@ public class SettingsActivity extends BaseActivity<ActivitySettingsBinding> {
     }
 
     private void initView() {
-        //getFragmentManager().beginTransaction().replace(R.id.settings_frame_layout, settingsFragment).commit();
+
+    }
+
+    public static class SettingsFragment extends PreferenceFragment implements Preference.OnPreferenceChangeListener, Preference.OnPreferenceClickListener {
+
+        private SwitchPreference weatherAlertsMorningPref;
+        private SwitchPreference weatherAlertsNightPref;
+
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.pref_settings);
+        }
+
+        @Override
+        public void onStart() {
+            super.onStart();
+            register();
+        }
+
+
+        @Override
+        public void onStop() {
+            super.onStop();
+        }
+
+        private void register() {
+            weatherAlertsMorningPref = (SwitchPreference) findPreference("pref_key_weather_alerts_morning");
+            weatherAlertsMorningPref.setOnPreferenceChangeListener(this);
+            weatherAlertsMorningPref.setOnPreferenceClickListener(this);
+            weatherAlertsNightPref = (SwitchPreference) findPreference("pref_key_weather_alerts_night");
+            weatherAlertsNightPref.setOnPreferenceChangeListener(this);
+            weatherAlertsNightPref.setOnPreferenceClickListener(this);
+        }
+
+        @Override
+        public boolean onPreferenceChange(Preference preference, Object newValue) {
+            SharedPreferences.Editor editor = MyApplication.getContext().getSharedPreferences("pref", MODE_PRIVATE).edit();
+            switch (preference.getKey()) {
+                case "pref_key_weather_alerts_morning":
+                    if (weatherAlertsMorningPref.isChecked()) {
+                        editor.putBoolean("pref_key_weather_alerts_morning", true);
+                    } else {
+                        editor.putBoolean("pref_key_weather_alerts_morning", false);
+                    }
+                    break;
+                case "pref_key_weather_alerts_night":
+                    if (weatherAlertsNightPref.isChecked()) {
+                        editor.putBoolean("pref_key_weather_alerts_night", true);
+                    } else {
+                        editor.putBoolean("pref_key_weather_alerts_night", false);
+                    }
+                    break;
+            }
+            editor.apply();
+            return true;
+        }
+
+        @Override
+        public boolean onPreferenceClick(Preference preference) {
+            final SharedPreferences.Editor editor = MyApplication.getContext().getSharedPreferences("pref", MODE_PRIVATE).edit();
+            switch (preference.getKey()) {
+                case "pref_key_weather_alerts_morning":
+                    if (weatherAlertsMorningPref.isChecked()) {
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                StringBuilder time = new StringBuilder();
+                                String strHourOfDay = hourOfDay < 10 ? "0" + hourOfDay : "" + hourOfDay;
+                                String strMinuteOfDay = minute < 10 ? "0" + minute : "" + minute;
+                                time.append(strHourOfDay + ":" + strMinuteOfDay);
+                                editor.putString("pref_key_weather_alters_time_morning", time.toString());
+                                weatherAlertsMorningPref.setSummaryOn(time);
+                            }
+                        }, 0, 0, true);
+                        timePickerDialog.show();
+                    }
+                    break;
+                case "pref_key_weather_alerts_night":
+                    if (weatherAlertsNightPref.isChecked()) {
+                        TimePickerDialog timePickerDialog = new TimePickerDialog(getActivity(), new TimePickerDialog.OnTimeSetListener() {
+                            @Override
+                            public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+                                StringBuilder time = new StringBuilder();
+                                String strHourOfDay = hourOfDay < 10 ? "0" + hourOfDay : "" + hourOfDay;
+                                String strMinuteOfDay = minute < 10 ? "0" + minute : "" + minute;
+                                time.append(strHourOfDay + ":" + strMinuteOfDay);
+                                editor.putString("pref_key_weather_alters_time_night", time.toString());
+                                weatherAlertsMorningPref.setSummaryOn(time);
+                            }
+                        }, 0, 0, true);
+                        timePickerDialog.show();
+                    }
+                    break;
+            }
+            editor.apply();
+            return true;
+        }
     }
 
 }
